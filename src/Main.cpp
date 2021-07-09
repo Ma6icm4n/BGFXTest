8f7158caf02ed18764b6a6ce237f0493e5ea7b9d
Magicman <clement.ciranna@viacesi.fr>
2021-07-09 21:45:43 +0200

    try

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,92 +1,143 @@
-#include <stdio.h>
-#include <bx/bx.h>
-#include <bgfx/bgfx.h>
-#include <bgfx/platform.h>
-#include <GLFW/glfw3.h>
-#if BX_PLATFORM_LINUX
-#define GLFW_EXPOSE_NATIVE_X11
-#elif BX_PLATFORM_WINDOWS
+#include <cstdio>
+#include "bgfx/bgfx.h"
+#include "bgfx/platform.h"
+#include "bx/math.h"
+#include "GLFW/glfw3.h"
 #define GLFW_EXPOSE_NATIVE_WIN32
-#elif BX_PLATFORM_OSX
-#define GLFW_EXPOSE_NATIVE_COCOA
-#endif
-#include <GLFW/glfw3native.h>
-#include "logo.h"
+#include "GLFW/glfw3native.h"
 
-static bool s_showStats = false;
+#define WNDW_WIDTH 1600
+#define WNDW_HEIGHT 900
 
-static void glfw_errorCallback(int error, const char* description)
+struct PosColorVertex
 {
-	fprintf(stderr, "GLFW error %d: %s\n", error, description);
+    float x;
+    float y;
+    float z;
+    uint32_t abgr;
+};
+
+static PosColorVertex cubeVertices[] =
+{
+    {-1.0f,  1.0f,  1.0f, 0xff000000 },
+    { 1.0f,  1.0f,  1.0f, 0xff0000ff },
+    {-1.0f, -1.0f,  1.0f, 0xff00ff00 },
+    { 1.0f, -1.0f,  1.0f, 0xff00ffff },
+    {-1.0f,  1.0f, -1.0f, 0xffff0000 },
+    { 1.0f,  1.0f, -1.0f, 0xffff00ff },
+    {-1.0f, -1.0f, -1.0f, 0xffffff00 },
+    { 1.0f, -1.0f, -1.0f, 0xffffffff },
+};
+
+static const uint16_t cubeTriList[] =
+{
+    0, 1, 2,
+    1, 3, 2,
+    4, 6, 5,
+    5, 6, 7,
+    0, 2, 4,
+    4, 2, 6,
+    1, 5, 3,
+    5, 7, 3,
+    0, 4, 1,
+    4, 5, 1,
+    2, 3, 6,
+    6, 3, 7,
+};
+
+bgfx::ShaderHandle loadShader(const char* FILENAME)
+{
+    const char* shaderPath = "???";
+
+    switch (bgfx::getRendererType()) {
+    case bgfx::RendererType::Noop:
+    case bgfx::RendererType::Direct3D9:  shaderPath = "../../externals/bgfx/examples/runtime/shaders/dx9/";   break;
+    case bgfx::RendererType::Direct3D11:
+    case bgfx::RendererType::Direct3D12: shaderPath = "../../externals/bgfx/examples/runtime/shaders/dx11/";  break;
+    case bgfx::RendererType::Gnm:        shaderPath = "../../externals/bgfx/examples/runtime/shaders/pssl/";  break;
+    case bgfx::RendererType::Metal:      shaderPath = "../../externals/bgfx/examples/runtime/shaders/metal/"; break;
+    case bgfx::RendererType::OpenGL:     shaderPath = "../../externals/bgfx/examples/runtime/shaders/glsl/";  break;
+    case bgfx::RendererType::OpenGLES:   shaderPath = "../../externals/bgfx/examples/runtime/shaders/essl/";  break;
+    case bgfx::RendererType::Vulkan:     shaderPath = "../../externals/bgfx/examples/runtime/shaders/spirv/"; break;
+    }
+
+    size_t shaderLen = strlen(shaderPath);
+    size_t fileLen = strlen(FILENAME);
+    char* filePath = (char*)calloc(1, shaderLen + fileLen +1);
+    memcpy(filePath, shaderPath, shaderLen);
+    memcpy(&filePath[shaderLen], FILENAME, fileLen);
+
+    FILE* file = fopen(filePath, "rb");
+    fseek(file, 0, SEEK_END);
+    long fileSize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    const bgfx::Memory* mem = bgfx::alloc(fileSize + 1);
+    fread(mem->data, 1, fileSize, file);
+    mem->data[mem->size - 1] = '\0';
+    fclose(file);
+
+    return bgfx::createShader(mem);
 }
 
-static void glfw_keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+int main(void)
 {
-	if (key == GLFW_KEY_F1 && action == GLFW_RELEASE)
-		s_showStats = !s_showStats;
-}
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(WNDW_WIDTH, WNDW_HEIGHT, "Hello, bgfx!", NULL, NULL);
 
-int main(int argc, char** argv)
-{
-	// Create a GLFW window without an OpenGL context.
-	glfwSetErrorCallback(glfw_errorCallback);
-	if (!glfwInit())
-		return 1;
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	GLFWwindow* window = glfwCreateWindow(1024, 768, "helloworld", nullptr, nullptr);
-	if (!window)
-		return 1;
-	glfwSetKeyCallback(window, glfw_keyCallback);
-	// Call bgfx::renderFrame before bgfx::init to signal to bgfx not to create a render thread.
-	// Most graphics APIs must be used on the same thread that created the window.
-	bgfx::renderFrame();
-	// Initialize bgfx using the native window handle and window resolution.
-	bgfx::Init init;
-#if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
-	init.platformData.ndt = glfwGetX11Display();
-	init.platformData.nwh = (void*)(uintptr_t)glfwGetX11Window(window);
-#elif BX_PLATFORM_OSX
-	init.platformData.nwh = glfwGetCocoaWindow(window);
-#elif BX_PLATFORM_WINDOWS
-	init.platformData.nwh = glfwGetWin32Window(window);
-#endif
-	int width, height;
-	glfwGetWindowSize(window, &width, &height);
-	init.resolution.width = (uint32_t)width;
-	init.resolution.height = (uint32_t)height;
-	init.resolution.reset = BGFX_RESET_VSYNC;
-	if (!bgfx::init(init))
-		return 1;
-	// Set view 0 to the same dimensions as the window and to clear the color buffer.
-	const bgfx::ViewId kClearView = 0;
-	bgfx::setViewClear(kClearView, BGFX_CLEAR_COLOR);
-	bgfx::setViewRect(kClearView, 0, 0, bgfx::BackbufferRatio::Equal);
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-		// Handle window resize.
-		int oldWidth = width, oldHeight = height;
-		glfwGetWindowSize(window, &width, &height);
-		if (width != oldWidth || height != oldHeight) {
-			bgfx::reset((uint32_t)width, (uint32_t)height, BGFX_RESET_VSYNC);
-			bgfx::setViewRect(kClearView, 0, 0, bgfx::BackbufferRatio::Equal);
-		}
-		// This dummy draw call is here to make sure that view 0 is cleared if no other draw calls are submitted to view 0.
-		bgfx::touch(kClearView);
-		// Use debug font to print information about this example.
-		bgfx::dbgTextClear();
-		bgfx::dbgTextImage(bx::max<uint16_t>(uint16_t(width / 2 / 8), 20) - 20, bx::max<uint16_t>(uint16_t(height / 2 / 16), 6) - 6, 40, 12, s_logo, 160);
-		bgfx::dbgTextPrintf(0, 0, 0x0f, "Press F1 to toggle stats.");
-		bgfx::dbgTextPrintf(0, 1, 0x0f, "Color can be changed with ANSI \x1b[9;me\x1b[10;ms\x1b[11;mc\x1b[12;ma\x1b[13;mp\x1b[14;me\x1b[0m code too.");
-		bgfx::dbgTextPrintf(80, 1, 0x0f, "\x1b[;0m    \x1b[;1m    \x1b[; 2m    \x1b[; 3m    \x1b[; 4m    \x1b[; 5m    \x1b[; 6m    \x1b[; 7m    \x1b[0m");
-		bgfx::dbgTextPrintf(80, 2, 0x0f, "\x1b[;8m    \x1b[;9m    \x1b[;10m    \x1b[;11m    \x1b[;12m    \x1b[;13m    \x1b[;14m    \x1b[;15m    \x1b[0m");
-		const bgfx::Stats* stats = bgfx::getStats();
-		bgfx::dbgTextPrintf(0, 2, 0x0f, "Backbuffer %dW x %dH in pixels, debug text %dW x %dH in characters.", stats->width, stats->height, stats->textWidth, stats->textHeight);
-		// Enable stats or debug text.
-		bgfx::setDebug(s_showStats ? BGFX_DEBUG_STATS : BGFX_DEBUG_TEXT);
-		// Advance to next frame. Process submitted rendering primitives.
-		bgfx::frame();
-	}
-	bgfx::shutdown();
-	glfwTerminate();
-	return 0;
+    bgfx::PlatformData pd;
+    pd.nwh = glfwGetWin32Window(window);
+    bgfx::setPlatformData(pd);
+
+    bgfx::Init bgfxInit;
+    bgfxInit.type = bgfx::RendererType::Count;
+    bgfxInit.resolution.width = WNDW_WIDTH;
+    bgfxInit.resolution.height = WNDW_HEIGHT;
+    bgfxInit.resolution.reset = BGFX_RESET_VSYNC;
+    bgfx::init(bgfxInit);
+
+    bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x443355FF, 1.0f, 0);
+    bgfx::setViewRect(0, 0, 0, WNDW_WIDTH, WNDW_HEIGHT);
+
+    bgfx::VertexLayout pcvDecl;
+    pcvDecl.begin()
+        .add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float)
+        .add(bgfx::Attrib::Color0, 4, bgfx::AttribType::Uint8, true)
+        .end();
+    bgfx::VertexBufferHandle vbh = bgfx::createVertexBuffer(bgfx::makeRef(cubeVertices, sizeof(cubeVertices)), pcvDecl);
+    bgfx::IndexBufferHandle ibh = bgfx::createIndexBuffer(bgfx::makeRef(cubeTriList, sizeof(cubeTriList)));
+
+    bgfx::ShaderHandle vsh = loadShader("vs_cubes.bin");
+    bgfx::ShaderHandle fsh = loadShader("fs_cubes.bin");
+    bgfx::ProgramHandle program = bgfx::createProgram(vsh, fsh, true);
+
+    unsigned int counter = 0;
+    while (true) {
+        const bx::Vec3 at = { 0.0f, 0.0f,  0.0f };
+        const bx::Vec3 eye = { 0.0f, 0.0f, -5.0f };
+        float view[16];
+        bx::mtxLookAt(view, eye, at);
+        float proj[16];
+        bx::mtxProj(proj, 60.0f, float(WNDW_WIDTH) / float(WNDW_HEIGHT), 0.1f, 100.0f, bgfx::getCaps()->homogeneousDepth);
+        bgfx::setViewTransform(0, view, proj);
+
+        float mtx[16];
+        bx::mtxRotateXY(mtx, counter * 0.01f, counter * 0.01f);
+        bgfx::setTransform(mtx);
+
+        bgfx::setVertexBuffer(0, vbh);
+        bgfx::setIndexBuffer(ibh);
+
+        bgfx::submit(0, program);
+        bgfx::frame();
+        counter++;
+    }
+
+    bgfx::destroy(ibh);
+    bgfx::destroy(vbh);
+    bgfx::shutdown();
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
