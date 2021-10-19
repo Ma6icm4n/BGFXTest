94d76831711554d5c2109fae49db83ea98bbf7cb
Ma6icm4n <cciranna@artfx.fr>
2021-10-19 11:59:02 +0200

    Add change path to exe work
    change path for the exe

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -5,14 +5,14 @@ bgfx::ShaderHandle loadShader(const char* filename) {
 
     switch (bgfx::getRendererType()) {
     case bgfx::RendererType::Noop:
-    case bgfx::RendererType::Direct3D9:  shaderPath = "../../externals/bgfx/examples/runtime/shaders/dx9/";   break;
+    case bgfx::RendererType::Direct3D9:  shaderPath = "../../../../../externals/bgfx/examples/runtime/shaders/dx9/";   break;
     case bgfx::RendererType::Direct3D11:
-    case bgfx::RendererType::Direct3D12: shaderPath = "../../externals/bgfx/examples/runtime/shaders/dx11/";  break;
-    case bgfx::RendererType::Gnm:        shaderPath = "../../externals/bgfx/examples/runtime/shaders/pssl/";  break;
-    case bgfx::RendererType::Metal:      shaderPath = "../../externals/bgfx/examples/runtime/shaders/metal/"; break;
-    case bgfx::RendererType::OpenGL:     shaderPath = "../../externals/bgfx/examples/runtime/shaders/glsl/";  break;
-    case bgfx::RendererType::OpenGLES:   shaderPath = "../../externals/bgfx/examples/runtime/shaders/essl/";  break;
-    case bgfx::RendererType::Vulkan:     shaderPath = "../../externals/bgfx/examples/runtime/shaders/spirv/"; break;
+    case bgfx::RendererType::Direct3D12: shaderPath = "../../../../../externals/bgfx/examples/runtime/shaders/dx11/";  break;
+    case bgfx::RendererType::Gnm:        shaderPath = "../../../../../externals/bgfx/examples/runtime/shaders/pssl/";  break;
+    case bgfx::RendererType::Metal:      shaderPath = "../../../../../externals/bgfx/examples/runtime/shaders/metal/"; break;
+    case bgfx::RendererType::OpenGL:     shaderPath = "../../../../../externals/bgfx/examples/runtime/shaders/glsl/";  break;
+    case bgfx::RendererType::OpenGLES:   shaderPath = "../../../../../externals/bgfx/examples/runtime/shaders/essl/";  break;
+    case bgfx::RendererType::Vulkan:     shaderPath = "../../../../../externals/bgfx/examples/runtime/shaders/spirv/"; break;
     }
 
     size_t shaderLen = strlen(shaderPath);
